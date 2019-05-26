df59d4e975f78254a0f38086a5c3d2cd0125fb0c
Maurits Dorlandt <mauritsdorlandt@Miranda.local>
2019-05-26 15:51:02 -0500

    css width adapted based on width summerbed

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(function() {
   $('input').change(function(){
-    $('#water-level').text(Math.round($("#flow-rate").val() / ($('#width').val() * 3.6) * 100) / 100)
+    // Calculate water level using flow rate and width
+    var width = $('#width').val()
+    var waterLevel = Math.round($("#flow-rate").val() / (width * 3.6) * 100) / 100
+
+    $('#water-level').text(waterLevel)
+    
+    var visualRiverWidth = (width / 1000) * 90
+    var visualRiverX = 50 - visualRiverWidth / 2
+
+    $('#flow-area, #water').css({ width: visualRiverWidth + '%', x: visualRiverX + '%' })
+
   })
 })
