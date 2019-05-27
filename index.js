ab832ef7f3289df56a19205877e4228dc9208f84
Maurits Dorlandt <mauritsdorlandt@Miranda.local>
2019-05-26 22:21:38 -0500

    Initial state Co-authored-by: Pelle ten Cate <pelle@technoot.nl>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('input').change(function(){
+  var calculateWaterLevelAndPaint = function(){
     // Calculate water level using flow rate and width
     var width = $('#width').val()
     var waterLevel = Math.round($("#flow-rate").val() / (width * 3.6) * 100) / 100
@@ -7,16 +7,10 @@ $(function() {
     $('#water-level').text(waterLevel)
     
     var visualRiverWidth = (width / 1000) * 90
-    var visualRiverX = 50 - visualRiverWidth / 2
-    
-    // vb   water level 4 meter dan is waar hij begint 50% van boven
-    // hoogte is =
-    
-    var visualWaterY = 90 - (waterLevel * 10)
-    var visualWaterHeight = waterLevel * 10
-
-    $('#flow-area, #water').css({ width: visualRiverWidth + '%', x: visualRiverX + '%' })
-    $('#water').css({ height: visualWaterHeight + '%', y: visualWaterY + '%'})
-
-  })
+    $('#flow-area, #water').css({ width: visualRiverWidth + '%'}).attr('x', 50 - visualRiverWidth / 2 + '%')
+    $('#water').css({ height: waterLevel * 10 + '%'}).attr('y', 75 - (waterLevel * 10) + '%')
+  }
+  
+  $('input').change(calculateWaterLevelAndPaint)
+  calculateWaterLevelAndPaint()
 })
