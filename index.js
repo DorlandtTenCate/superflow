c80cfc9295fb4c5d60ce1d34c325bf5787f32c43
Maurits Dorlandt <mauritsdorlandt@Miranda.local>
2019-05-27 11:45:57 -0500

    make water move with depth summerbed
    Co-authored-by: Pelle ten Cate <pelle@technoot.nl>

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ $(function() {
     var width = $('#width').val()
     var waterLevel = Math.round($("#flow-rate").val() / (width * 3.6) * 100) / 100
 
+    // Display water level in text
     $('#water-level').text(waterLevel)
     
+    // Calculated as percentage from meters
     var visualRiverWidth = (width / 1000) * 90
+    var visualFlowAreaHeight = $('#depth').val() * 10
+    var visualWaterHeight = waterLevel * 10
+    var visualWaterTop = 50 + visualFlowAreaHeight - visualWaterHeight
+
     $('#flow-area, #water').css({ width: visualRiverWidth + '%'}).attr('x', 50 - visualRiverWidth / 2 + '%')
-    $('#water').css({ height: waterLevel * 10 + '%'}).attr('y', 75 - (waterLevel * 10) + '%')
+    $('#flow-area').css({ height: visualFlowAreaHeight + '%'}).attr('y' + '%')
+    $('#water').css({ height: visualWaterHeight + '%'}).attr('y', visualWaterTop + '%')
   }
-  
+
   $('input').change(calculateWaterLevelAndPaint)
   calculateWaterLevelAndPaint()
 })
