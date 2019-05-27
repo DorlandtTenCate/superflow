ce9596485f144ef293cfddae728777c5d9e60183
Maurits Dorlandt <mauritsdorlandt@Miranda.local>
2019-05-26 19:32:58 -0500

    make the image change with the water level

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ $(function() {
     
     var visualRiverWidth = (width / 1000) * 90
     var visualRiverX = 50 - visualRiverWidth / 2
+    
+    // vb   water level 4 meter dan is waar hij begint 50% van boven
+    // hoogte is =
+    
+    var visualWaterY = 90 - (waterLevel * 10)
+    var visualWaterHeight = waterLevel * 10
 
     $('#flow-area, #water').css({ width: visualRiverWidth + '%', x: visualRiverX + '%' })
+    $('#water').css({ height: visualWaterHeight + '%', y: visualWaterY + '%'})
 
   })
 })
