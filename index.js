24aefbfc3a4b8d4d6df5a4b9c91acecde139898f
Maurits Dorlandt <mauritsdorlandt@Miranda.local>
2019-05-31 13:52:30 -0500

    flow area is arc-ed

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ $(function() {
     $('#water-level').text(waterLevel)
     
     // Calculated as percentage from meters
-    var visualRiverWidth = (width / 1000) * 90
-    var visualFlowAreaHeight = $('#depth').val() * 10
-    var visualWaterHeight = waterLevel * 10
+    var visualRiverWidth = (width / 1000) * 900
+    var visualFlowAreaHeight = $('#depth').val() * 100
+    var visualWaterHeight = waterLevel * 100
     var visualWaterTop = 50 + visualFlowAreaHeight - visualWaterHeight
 
-    $('#flow-area, #water').css({ width: visualRiverWidth + '%'}).attr('x', 50 - visualRiverWidth / 2 + '%')
-    $('#flow-area').css({ height: visualFlowAreaHeight + '%'}).attr('y' + '%')
-    $('#water').css({ height: visualWaterHeight + '%'}).attr('y', visualWaterTop + '%')
+    $('#flow-area').attr('d', ['M', 500 - visualRiverWidth / 2, 500, 'a', visualRiverWidth / 2, visualFlowAreaHeight, 0, 0, 0, visualRiverWidth, 0].join(' '))
+
+    // $('#flow-area, #water').css({ width: visualFlowAreaWidth + '%'}).attr('x', 50 - visualRiverWidth / 2 + '%')
+    // $('#flow-area').css({ height: visualFlowAreaHeight + '%'}).attr('y' + '%')
+    // $('#water').css({ height: visualWaterHeight + '%'}).attr('y', visualWaterTop + '%')
   }
 
   $('input').change(calculateWaterLevelAndPaint)
