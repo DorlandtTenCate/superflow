658711fad498a4840c51a0986d51e3b4412191d3
Pelle ten Cate <pelletencate@gmail.com>
2019-10-08 11:53:10 +0200

    Round water. (And refactors)

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 $(function() {
-  var calculateWaterLevelAndPaint = function(){
+  var calculateWaterLevelAndPaint = function() {
     // Calculate water level using flow rate and width
-    var width = $('#width').val()
-    var waterLevel = Math.round($("#flow-rate").val() / (width * 3.6) * 100) / 100
+    var widthM = $('#width').val()
+    var waterLevelM = Math.round($("#flow-rate").val() / (widthM * 3.6) * 100) / 100
 
     // Display water level in text
-    $('#water-level').text(waterLevel)
-    
-    // Calculated as percentage from meters
-    var visualRiverWidth = (width / 1000) * 900
-    var visualFlowAreaHeight = $('#depth').val() * 100
-    var visualWaterHeight = waterLevel * 100
-    var visualWaterTop = 50 + visualFlowAreaHeight - visualWaterHeight
+    $('#water-level').text(waterLevelM)
 
-    $('#flow-area').attr('d', ['M', 500 - visualRiverWidth / 2, 500, 'a', visualRiverWidth / 2, visualFlowAreaHeight, 0, 0, 0, visualRiverWidth, 0].join(' '))
+    var riverWidthU = (widthM / 1000) * 900
+    var flowAreaHeightU = $('#depth').val() * 100
+    var waterHeightU = waterLevelM * 100
+    var waterTopU = 500 + flowAreaHeightU - waterHeightU
+    var waterWidthU = riverWidthU * Math.sin(waterHeightU / flowAreaHeightU * 0.5 * Math.PI)
 
-    // $('#flow-area, #water').css({ width: visualFlowAreaWidth + '%'}).attr('x', 50 - visualRiverWidth / 2 + '%')
-    // $('#flow-area').css({ height: visualFlowAreaHeight + '%'}).attr('y' + '%')
-    // $('#water').css({ height: visualWaterHeight + '%'}).attr('y', visualWaterTop + '%')
+    $('#flow-area').attr('d', ['M', 500 - riverWidthU / 2, 500, 'a', riverWidthU / 2, flowAreaHeightU, 0, 0, 0, riverWidthU, 0].join(' '))
+    // $('#water').attr('d', ['M', 500 - waterWidthU / 2, 500 + (flowAreaHeightU - waterHeightU), 'a', waterWidthU / 2, waterHeightU, 0, 0, 0, waterWidthU, 0].join(' '))
+    $('#flow-area, #water').css({ width: riverWidthU / 10 + '%'}).attr('x', 50 - riverWidthU / 20 + '%')
+    $('#flow-area').css({ height: flowAreaHeightU + '%'}).attr('y' + '%')
+    $('#water').css({ height: waterHeightU / 10 + '%'}).attr('y', waterTopU / 10 + '%')
   }
 
   $('input').change(calculateWaterLevelAndPaint)
